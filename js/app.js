07d4b9f74cae238a0de55b484d713144c19560a6
Kristinn <kmcnair_100@outlook.com>
2023-08-31 15:58:57 -0400

    edit scrolling feature

--- js/app.js
+++ js/app.js
@@ -1,47 +1,40 @@
-function menuToggle(){
-    let nav = document.getElementById("nav");
-    nav.classList.toggle('active')
-    let toggle = document.getElementById("toggle");
-    toggle.classList.toggle('active') 
+// Smooth scroll function
+function smoothScroll(target, duration) {
+  const targetSection = document.querySelector(target);
+  if (!targetSection) return;
+
+  const targetPosition = targetSection.getBoundingClientRect().top;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
 }
 
-function smoothScroll (target, duration) {
-    var target = document.querySelector(target);
-    var targetPosition = target.getBoundingClientRect().top;
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav__link");
 
-    function animation(currentTime){
-        if(startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0,run);
-        if (timeElapsed < duration) requestAnimationFrame(animation);
-    }
-
-    function ease(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t* (t - 2) - 1) + b;
-    }
-
-    requestAnimationFrame(animation);
-}
-
-
-const scroll2 = document.querySelector('.scroll2');
-scroll2.addEventListener('click', function(){
-    smoothScroll('#section2', 1500);
-});
-
-const scroll3 = document.querySelector('.scroll3');
-scroll3.addEventListener('click', function(){
-    smoothScroll('#section3', 1750);
-});
-
-const scroll4 = document.querySelector('.scroll4');
-scroll4.addEventListener('click', function(){
-    smoothScroll('#section4', 2000);
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetSectionId = link.getAttribute("href");
+      const duration = parseInt(link.getAttribute("data-duration")) || 1000; // Default duration of 1000ms
+      smoothScroll(targetSectionId, duration);
+    });
+  });
 });
